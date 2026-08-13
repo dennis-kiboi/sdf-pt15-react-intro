b73c5a007969a4e8f1f452ae2c2474d23d35d2b0
Dennis Kiboi <dennis.kiboi@moringaschool.com>
2026-08-13 21:09:08 +0300

    13-08 Added styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,9 +2,9 @@ const Card = (props) => {
   console.log(props)
 
   return (
-    <div className="card">
-      <p>Name: {props.name}</p>
-      <small>Email: {props.email} </small>
+    <div className="rounded p-5 bg-gray-400 m-5">
+      <p className="text-red-600">Name: {props.name}</p>
+      <small className="font-bold">Email: {props.email} </small>
     </div>
   );
 };
